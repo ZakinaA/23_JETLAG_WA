81ecf2f0f51b5a5c59d2beb3a1a151152318282c
JanekBigoin <Bigoinjanek@gmail.com>
2023-09-25 15:19:48 +0200

    Ajout de Sport

---
+++ src/main/java/bts/sio/webapp/repository/SportRepository.java
@@ -0,0 +1,127 @@
+package bts.sio.webapp.repository;
+
+
+import bts.sio.webapp.CustomProperties;
+import bts.sio.webapp.model.Sport;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Component
+public class SportRepository {
+    @Autowired
+    private CustomProperties props;
+
+    /**
+     * Get all athletes
+     * @return An iterable of all athlete
+     */
+    public Iterable<Sport> getSports() {
+
+        String baseApiUrl = props.getApiUrl();
+        String getSportsUrl = baseApiUrl + "/sports";
+        System.out.println("url=" + getSportsUrl);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Iterable<Sport>> response = restTemplate.exchange(
+                getSportsUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<Iterable<Sport>>() {}
+        );
+
+        log.debug("Get Sports call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+
+    /**
+     * Get an athlete by the id
+     * @param id The id of the athlete
+     * @return The athlete which matches the id
+     */
+    public Sport getSport(int id) {
+        String baseApiUrl = props.getApiUrl();
+        String getSportUrl = baseApiUrl + "/Sport/" + id;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Sport> response = restTemplate.exchange(
+                getSportUrl,
+                HttpMethod.GET,
+                null,
+                Sport.class
+        );
+
+        log.debug("Get Sport call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+
+    /**
+     * Add a new athlete
+     * @param a A new athlete (without an id)
+     * @return The athlete full filled (with an id)
+     */
+    public Sport createSport(Sport a) {
+
+        String baseApiUrl = props.getApiUrl();
+        String createSportUrl = baseApiUrl + "/Sport";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Sport> request = new HttpEntity<Sport>(a);
+        ResponseEntity<Sport> response = restTemplate.exchange(
+                createSportUrl,
+                HttpMethod.POST,
+                request,
+                Sport.class);
+
+        log.debug("Create Sport call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+
+    /**
+     * Update an athlete - using the PUT HTTP Method.
+     * @param e Existing athlete to update
+     */
+    public Sport updateSport(Sport e) {
+        String baseApiUrl = props.getApiUrl();
+        String updateSportUrl = baseApiUrl + "/Sport/" + e.getId();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Sport> request = new HttpEntity<Sport>(e);
+        ResponseEntity<Sport> response = restTemplate.exchange(
+                updateSportUrl,
+                HttpMethod.PUT,
+                request,
+                Sport.class);
+
+        log.debug("Update Sport call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+
+    /*
+     * Delete an athlete using exchange method of RestTemplate
+     * instead of delete method in order to log the response status code.
+     * @param e The athlete to delete
+     */
+    public void deleteSport(int id) {
+        String baseApiUrl = props.getApiUrl();
+        String deleteSportUrl = baseApiUrl + "/Sport/" + id;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Void> response = restTemplate.exchange(
+                deleteSportUrl,
+                HttpMethod.DELETE,
+                null,
+                Void.class);
+
+        log.debug("Delete Sport call " + response.getStatusCode().toString());
+    }
+}
---
+++ src/main/java/bts/sio/webapp/service/SportService.java
@@ -0,0 +1,44 @@
+package bts.sio.webapp.service;
+
+import bts.sio.webapp.model.Sport;
+import bts.sio.webapp.repository.SportRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Data
+@Service
+public class SportService {
+
+    @Autowired
+    private SportRepository sportRepository;
+
+    public Sport getSport(final int id) {
+        return sportRepository.getSport(id);
+    }
+
+    public Iterable<Sport> getSports() {
+        return sportRepository.getSports();
+    }
+
+    public void deleteSport(final int id) {
+        sportRepository.deleteSport(id);
+    }
+
+    public Sport saveSport(Sport sport) {
+        Sport savedSport;
+
+        // Functional rule : Last name must be capitalized.
+        sport.setNom(sport.getNom().toUpperCase());
+        sport.setDescriptif(sport.getDescriptif().toUpperCase());
+
+        if(sport.getId() == null) {
+            // If id is null, then it is a new employee.
+            savedSport = sportRepository.createSport(sport);
+        } else {
+            savedSport = sportRepository.updateSport(sport);
+        }
+
+        return savedSport;
+    }
+}
