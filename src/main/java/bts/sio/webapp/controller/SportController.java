7c69644c467ca11034f70dddf98b5dd6992dcf2f
VillainEliot <eliot.villain240404@gmail.com>
2023-10-09 16:59:19 +0200

    ajout fonctionnalité ajouter un sport

--- src/main/java/bts/sio/webapp/controller/SportController.java
+++ src/main/java/bts/sio/webapp/controller/SportController.java
@@ -1,6 +1,10 @@
 package bts.sio.webapp.controller;
 
+import bts.sio.webapp.model.Athlete;
+import bts.sio.webapp.model.Olympiade;
+import bts.sio.webapp.model.Pays;
 import bts.sio.webapp.model.Sport;
+import bts.sio.webapp.service.OlympiadeService;
 import bts.sio.webapp.service.SportService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +29,15 @@ public class SportController {
         model.addAttribute("sports", listSports);
         return "sport/listeSport";
     }
+
+    @GetMapping("/createSport")
+    public String createSport(Model model) {
+        Sport a = new Sport();
+        model.addAttribute("sport", a);
+
+        return "sport/formNewSport";
+    }
+
     @GetMapping("/deleteSport/{id}")
     public ModelAndView deleteSport(@PathVariable("id") final int id) {
         sportservice.deleteSport(id);
