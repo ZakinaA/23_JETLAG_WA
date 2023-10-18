91a8f17397d6d32472e75872ef397e0aa6bfff38
BouquetThomas <darkelixor@gmail.com>
2023-10-18 10:55:33 +0200

    Ajouter une Epreuve à un Sport

--- src/main/java/bts/sio/webapp/controller/EpreuveController.java
+++ src/main/java/bts/sio/webapp/controller/EpreuveController.java
@@ -1,13 +1,19 @@
 package bts.sio.webapp.controller;
 
 import bts.sio.webapp.model.Epreuve;
+import bts.sio.webapp.model.Sport;
 import bts.sio.webapp.service.EpreuveService;
+
+
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Data
@@ -16,6 +22,29 @@ public class EpreuveController {
 
     @Autowired
     private EpreuveService epreuveService;
+
+    @GetMapping("/createSportEpreuve")
+    public String createSportEpreuve(@RequestParam(name = "sport_id") Long sport_id,
+                            Model model) {
+        Epreuve epreuve = new Epreuve();
+        Sport s = new Sport();
+        s.setId(sport_id);
+        epreuve.setSport(s);
+
+        model.addAttribute("epreuve", epreuve);
+        model.addAttribute("sport_id", sport_id);
+        return "sportEpreuves/formNewSportEpreuve";
+    }
+
+    @PostMapping("/saveEpreuve")
+    public ModelAndView saveEpreuve(@ModelAttribute Epreuve epreuve) {
+        System.out.println("controller save=" + epreuve.getLibelle());
+        if(epreuve.getId() != null) {
+            Epreuve current = epreuveService.getEpreuve(epreuve.getId());
+        }
+        epreuveService.saveEpreuve(epreuve);
+        return new ModelAndView("redirect:/sports");
+    }
 
     @GetMapping("/epreuves")
     public String Epreuve(@RequestParam(name = "sport_id") Long sport_id,
--- src/main/java/bts/sio/webapp/model/Sport.java
+++ src/main/java/bts/sio/webapp/model/Sport.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class Sport {
 
-    private Integer id;
+    private Long id;
     private String nom;
     private String descriptif;
     private String nom_image;
